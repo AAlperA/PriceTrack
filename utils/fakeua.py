4d369d63550c11a0e3bdd5be427a4f01b304f6f8
AAlperA <aalperagaoglu@hotmail.com>
2025-08-22 04:26:26 +0300

    improve set_uagent function

--- utils/fakeua.py
+++ utils/fakeua.py
@@ -1,8 +1,11 @@
 import random
 
 def set_uagent():
-    return (
-        f"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 " 
-        f"(KHTML, like Gecko) Chrome/123.0.0.{random.randint(0, 9999)}" 
-        f" Safari/537.{random.randint(0, 999)}"
-    )
+    platforms = [
+        "Windows NT 10.0; Win64; x64",
+        "Macintosh; Intel Mac OS X 10_15_7",
+        "X11; Linux x86_64"
+    ]
+    chrome_version = f"{random.randint(115, 118)}.0.{random.randint(5000, 5999)}.{random.randint(0, 150)}"
+    platform = random.choice(platforms)
+    return f"Mozilla/5.0 ({platform}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{chrome_version} Safari/537.36"
